23621ada881b49486de68c0cd0bb37f5b2e47183
C. M. de Picciotto <d3p1@d3p1.dev>
2025-08-18 02:31:36 +0000

    feat: add leva and performance tool [#1]

--- src/js/component/App.tsx
+++ src/js/component/App.tsx
@@ -4,17 +4,63 @@
  */
 import {Canvas} from '@react-three/fiber'
 import {Float, OrbitControls, Text} from '@react-three/drei'
+import {Leva, useControls} from 'leva'
+import {Perf} from 'r3f-perf'
 
 export const App = () => {
-  return (
-    <Canvas camera={{position: [0, 1, 3]}}>
-      <OrbitControls makeDefault={true} />
+  const {isPerfVisible} = useControls('perf', {
+    isPerfVisible: false,
+  })
 
-      <Float speed={2} floatIntensity={4}>
-        <Text fontSize={0.2} color="black">
-          Scroll Animation Coming Soon 🕹
-        </Text>
-      </Float>
-    </Canvas>
+  const {
+    floatSpeed,
+    floatIntensity,
+    fontSize,
+    fontColor,
+    isTextVisible,
+    content,
+  } = useControls('text', {
+    floatSpeed: {
+      value: 2,
+      min: 0,
+      max: 10,
+      step: 0.1,
+    },
+    floatIntensity: {
+      value: 4,
+      min: 0,
+      max: 10,
+      step: 0.1,
+    },
+    fontSize: {
+      value: 0.2,
+      min: 0.1,
+      max: 4,
+      step: 0.01,
+    },
+    fontColor: '#000000',
+    isTextVisible: true,
+    content: {
+      options: ['🤪', '🕹', '😎'],
+    },
+  })
+
+  return (
+    <>
+      <Leva collapsed={true} />
+      <Canvas camera={{position: [0, 1, 3]}}>
+        {isPerfVisible && <Perf position="top-left" />}
+
+        <OrbitControls makeDefault={true} />
+
+        {isTextVisible && (
+          <Float speed={floatSpeed} floatIntensity={floatIntensity}>
+            <Text fontSize={fontSize} color={fontColor}>
+              {content}
+            </Text>
+          </Float>
+        )}
+      </Canvas>
+    </>
   )
 }
